7743767ed392fa3476de242cc2038c8f25a4f7a7
Pasha Faisal <ahmedpashafaisal@gmail.com>
2026-09-22 18:38:21 +0300

    Remove synopses from coming-soon stories

--- src/components/StoryModal.jsx
+++ src/components/StoryModal.jsx
@@ -38,7 +38,7 @@ function StoryModal({ story, themes, progress, signedIn, onSignIn, onClose }) {
           <div className="modal-body">
             <h2 id="story-modal-title">{story.title}</h2>
             <p className="modal-tagline">{story.tagline}</p>
-            <p className="modal-synopsis">{story.synopsis}</p>
+            {story.synopsis && <p className="modal-synopsis">{story.synopsis}</p>}
             <SkillTags skills={story.skills} />
 
             <ul className="modal-facts">
